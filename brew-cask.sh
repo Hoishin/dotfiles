41a2607010b15120308de8994c0510bb0d778ef0
hoishin <hoishinxii@gmail.com>
2018-06-15 14:42:47 +0900

    modify brew; add brew-cask

---
+++ brew-cask.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+brew tap homebrew/cask
+
+brew cask install \
+	1password \
+	1password-cli \
+	avast-security \
+	discord \
+	docker \
+	gimp \
+	gitkraken \
+	google-chrome \
+	google-drive-file-stream \
+	google-japanese-ime \
+	hyperswitch \
+	insomnia \
+	iterm2 \
+	java \
+	kap \
+	karabiner-elements \
+	postico \
+	slack \
+	spotify \
+	vagrant \
+	vagrant-manager \
+	virtualbox \
+	visual-studio-code
